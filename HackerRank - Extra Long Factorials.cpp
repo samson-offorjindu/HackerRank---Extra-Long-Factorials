a21b3b1c034d516f1ff35390dcf4d2525401f758
Samson Offorjindu <76940072+samson-offorjindu@users.noreply.github.com>
2023-01-09 23:02:41 +0100

    Add project files.

---
+++ HackerRank - Extra Long Factorials.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long extraLongFactorials(int n) {
+    int len = 1;
+    vector<short> num{ 1 };
+    for (int i = 1; i <= n; i++) {
+        int c = 0;
+        for (int j = 0; j < len; j++) {
+            int k = num[j] * i + c;
+            num[j] = k % 10;
+            c = k / 10;
+        }
+        while (c) {
+            num.push_back(c % 10);
+            c /= 10;
+            len++;
+        }
+    }
+    for (int i = num.size() - 1; i > -1; i--) {
+        cout << num[i];
+    }
+}
+
+int main() {
+    cout << extraLongFactorials(25);
+
+    EXIT_SUCCESS;
+}
